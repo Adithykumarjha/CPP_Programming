b81ec1e8ad7b232aff19d62f60f0bbfc8ed0dc36
Adithy Kumar Jha <147861071+Adithykumarjha@users.noreply.github.com>
2026-06-25 11:13:05 +0530

    added new program file

---
+++ ListInsertingElements.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+int main(){
+	list<int>lst1,lst2;
+	int i;
+	for(int i=0;i<10;i++){
+		lst1.push_back(i);
+	}
+	
+	for(int i=0;i<10;i++){
+		lst2.push_front(i);
+	}
+	
+	list<int>::iterator p;
+	
+	cout<<"Contents of list1:\n";
+	
+	p=lst1.begin();
+	
+	while(p!=lst1.end()){
+		cout<<*p<<" ";
+		p++;
+	}
+	cout<<"\n\n";
+	
+	cout<<"Contents of list2:\n ";
+	p=lst2.begin();
+	while(p!=lst2.end()){
+		cout<<*p<<" ";
+		p++;
+	}
+	
+	return 0;
+	
+}
